a335c7425697a520320c0565ea370eef4e4b8ce5
Gabriel Soares Martins <gmartins@padtec.com.br>
2018-04-04 15:34:07 -0300

    Support to blacklist projects.
    Change-Id: I4d5cc84719d7bab4f028995f722d7e9f3119a167

--- bin/gentagdb.sh
+++ bin/gentagdb.sh
@@ -10,13 +10,33 @@ if [[ ! -d ${CSCOPE_FILES} ]]; then
   mkdir "${CSCOPE_FILES}"
 fi
 
+blacklist() {
+    cat << EOF
+refs.*build\|\
+padtec.*build\|\
+build.*padtec\|\
+trd.*build
+EOF
+}
+
+use_blacklist() {
+  echo -n > "${CSCOPE_FILES}"/cscope.files
+  while read list; do
+    if [ x${1} == xno ]; then
+        echo ${list} >> "${CSCOPE_FILES}"/cscope.files
+    else
+        echo ${list} | grep -v "$(blacklist)" >> "${CSCOPE_FILES}"/cscope.files
+    fi
+  done
+}
+
 find "$DIR" -name "*.c" -o \
 -name "*.cc" -o \
 -name "*.cpp" -o \
 -name "*.h" -o \
 -name "*.hh" -o \
 -name "*.hpp" \
--type f | grep -v "refs.*build\|padtec.*build\|build.*padtec\|trd.*build" > "${CSCOPE_FILES}"/cscope.files
+-type f | xargs realpath | $(use_blacklist ${2})
 
 cd "${CSCOPE_FILES}"
 cscope -q -R -b -k
