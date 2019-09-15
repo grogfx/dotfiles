71b6887f1a25886c0dfdb887e6b72571ee3fe07a
Gabriel Soares Martins <gsmartinsbr@gmail.com>
2019-09-15 11:33:19 -0300

    Add bash-completion to the packages list and more
    Fix USER_HOME variable on env-setup script for systems that don't have
    that variable.
    
    Remove trailing spaces and some commented command on .i3/config.

--- env-setup.sh
+++ env-setup.sh
@@ -6,12 +6,14 @@ PACKAGES_LIST="i3 \
     rxvt-unicode \
     feh \
     xautolock \
+    bash-completion \
     fonts-croscore"
 
 install_packages() {
     sudo apt install ${PACKAGES_LIST}
 }
 
+USER_HOME=${USER_HOME:-/home/${USER}}
 update_xresources() {
     xrdb -merge ${USER_HOME}/.Xresources
     xrdb -load ${USER_HOME}/.Xresources
